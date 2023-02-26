23fcf47e0c1877a0edc948510cdb61dd9b10b503
Jordi Morreres <jmorreres@andorra.ad>
2023-02-26 11:39:49 +0000

    Instruments HoneyComb

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -38,6 +38,12 @@ tracer = trace.get_tracer(__name__)
 
 
 app = Flask(__name__)
+
+# HoneyComb ---------
+# Initialize automatic instrumentation with Flask
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
