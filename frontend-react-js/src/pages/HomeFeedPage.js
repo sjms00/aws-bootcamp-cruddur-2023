c0698e7102639751d2a65cafe9ef866fca981a1d
Jordi Morreres <jmorreres@andorra.ad>
2023-03-06 17:42:56 +0000

    upd apps

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -1,6 +1,8 @@
 import './HomeFeedPage.css';
 import React from "react";
 
+import { Auth } from 'aws-amplify';
+
 import DesktopNavigation  from '../components/DesktopNavigation';
 import DesktopSidebar     from '../components/DesktopSidebar';
 import ActivityFeed from '../components/ActivityFeed';
@@ -50,22 +52,24 @@ export default function HomeFeedPage() {
     }
   };
 
+  // check if we are authenicated
   const checkAuth = async () => {
-    console.log('checkAuth')
-    // [TODO] Authenication
-    if (Cookies.get('user.logged_in')) {
-      setUser({
-        display_name: Cookies.get('user.name'),
-        handle: Cookies.get('user.username')
-      })
-      // Honeycomb frontend
-      tracer.startActiveSpan('User informed', usrSpan => {
-        // Add your attributes to describe the button clicked here
-        usrSpan.setAttribute('User: ', Cookies.get('user.name'));
-  
-        usrSpan.end();
-      });
-      }
+    Auth.currentAuthenticatedUser({
+      // Optional, By default is false. 
+      // If set to true, this call will send a 
+      // request to Cognito to get the latest user data
+      bypassCache: false 
+    })
+    .then((user) => {
+      console.log('user',user);
+      return Auth.currentAuthenticatedUser()
+    }).then((cognito_user) => {
+        setUser({
+          display_name: cognito_user.attributes.name,
+          handle: cognito_user.attributes.preferred_username
+        })
+    })
+    .catch((err) => console.log(err));
   };
 
   React.useEffect(()=>{
